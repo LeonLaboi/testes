c496605489f2fe98b2e8e3ddcdfc6e84400a3c7f
Claudio Meinberg <claudio.meinberg@gmail.com>
2023-06-27 16:54:42 -0300

    feat: enable admin for exemplo_model

--- testes_django/exemplo_model/admin.py
+++ testes_django/exemplo_model/admin.py
@@ -1,3 +1,56 @@
 from django.contrib import admin
+from .models import Scan, Experiment
 
-# Register your models here.
+
+@admin.register(Scan)
+class ScanAdmin(admin.ModelAdmin):
+
+    list_display = [
+        'name',
+        'last_run',
+        'auto_run',
+    ]
+
+    list_display_links = [
+        'name',
+        'last_run',
+
+    ]
+
+    search_fields = [
+        'description',
+    ]
+
+    list_filter = [
+        'last_run',
+        'auto_run',
+    ]
+
+    ordering = ['name', ]
+
+
+@admin.register(Experiment)
+class ExperimentAdmin(admin.ModelAdmin):
+
+    list_display = [
+        'name',
+        'scan',
+        'bar_size',
+    ]
+
+    list_display_links = [
+        'name',
+        'scan',
+        'bar_size',
+    ]
+
+    search_fields = [
+        'description',
+    ]
+
+    list_filter = [
+        'scan',
+        'bar_size',
+    ]
+
+    ordering = ['name', ]
